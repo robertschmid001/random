ccec115d054b28fd72fe7863ebab9b6a9ec7b99e
Robert.SCHMID <robertschmid001@gmail.com>
2018-10-08 17:24:12 +0200

    added tables holding / entreprise

--- src/components/Portefeuille/_subs/Sante.vue
+++ src/components/Portefeuille/_subs/Sante.vue
@@ -1,22 +1,39 @@
 <template>
   <div id="sante">
     <div class="wrapping-search">
-      <el-button class="button inner-button"  @click="toggleSelection([tableData3[1], tableData3[2]])">En cours</el-button>
+      <el-button class="button inner-button"  @click="toggleSelection([holdings[1], holdings[2]])">En cours</el-button>
       <el-button class="button inner-button" @click="toggleSelection()">Résiliés</el-button>
     </div>
-    <div class="table-wrapping">
-    <el-table ref="multipleTable" :data="tableData3" style="width: 100%; font-size: 10px;" @selection-change="handleSelectionChange" :stripe="true" :highlight-current-row="true" :default-sort = "{prop: 'nom', order: 'ascending'}">
-      <el-table-column type="selection" width="42"></el-table-column>
-      <el-table-column property="nom" label="NOM" sortable width="100"></el-table-column>
-      <el-table-column property="entreprises" label="ENTREPRISES" sortable width="120"></el-table-column>
-      <el-table-column property="contrats" label="CONTRATS" show-overflow-tooltip sortable width="100"></el-table-column>
-      <el-table-column property="assuresBeneficiaires" label="ASSURES / BENEFICIAIRES" show-overflow-tooltip width="179"></el-table-column>
-      <el-table-column property="cotisations" label="COTISATIONS" show-overflow-tooltip sortable width="120"></el-table-column>
-      <el-table-column property="prestations" label="PRESTATIONS" show-overflow-tooltip sortable width="120"></el-table-column>
-      <el-table-column property="tauxTeletransmissions" label="TAUX TELETRANSMISSION" show-overflow-tooltip sortable><template scope="scope"><el-progress :text-inside="true" :stroke-width="18" :percentage="scope.row.tauxTeletransmissions"></el-progress></template></el-table-column>
-      <el-table-column property="documents" label="DOCUMENTS" show-overflow-tooltip width="85" style="text-align: center;"><template scope="scope"><font-awesome-icon v-if="scope.row.documents" icon="download" class="size-export"/></template></el-table-column>
-    </el-table>
+    <!-- <template scope="scope"></template> -->
+    <div class="table-wrapping" v-show="holdingTable">
+      <el-table ref="multipleTable" :data="holdings" style="width: 100%; font-size: 10px;" @selection-change="handleSelectionChange" :stripe="true" :highlight-current-row="true" :default-sort = "{prop: 'nom', order: 'ascending'}">
+        <el-table-column type="selection" width="42"></el-table-column>
+        <el-table-column property="name" label="NOM" sortable width="100"><template scope="scope" >{{scope.row.name}}</template></el-table-column>
+        <el-table-column property="entreprises.length" label="ENTREPRISES" sortable width="120"><template scope="scope" ><div @click="showEntreprise(scope.row.entreprises)" class="data-wrapper">{{scope.row.entreprises.length}}</div></template></el-table-column>
+        <el-table-column property="contrats" label="CONTRATS" show-overflow-tooltip sortable width="100"></el-table-column>
+        <el-table-column property="beneficiaire" label="ASSURES / BENEFICIAIRES" show-overflow-tooltip width="179"></el-table-column>
+        <el-table-column property="cotisations" label="COTISATIONS" show-overflow-tooltip sortable width="120"></el-table-column>
+        <el-table-column property="prestations" label="PRESTATIONS" show-overflow-tooltip sortable width="120"></el-table-column>
+        <el-table-column property="tauxTeletransmissions" label="TAUX TELETRANSMISSION" show-overflow-tooltip sortable><template scope="scope"><el-progress :text-inside="true" :stroke-width="18" :percentage="scope.row.tauxTeletransmissions"></el-progress></template></el-table-column>
+        <el-table-column property="documents" label="DOCUMENTS" show-overflow-tooltip width="85" style="text-align: center;"><template scope="scope"><font-awesome-icon v-if="scope.row.documents" icon="download" class="size-export"/></template></el-table-column>
+      </el-table>
     </div>
+    <div class="table-wrapping" v-show="entrepriseTable">
+      <el-table ref="multipleTable" :data="entreprises" style="width: 100%; font-size: 10px;" @selection-change="handleSelectionChange" :stripe="true" :highlight-current-row="true" :default-sort = "{prop: 'nom', order: 'ascending'}">
+        <el-table-column type="selection" width="42"></el-table-column>
+        <el-table-column property="name" label="NOM" sortable width="100"></el-table-column>
+        <el-table-column property="entreprises" label="ENTREPRISES" sortable width="120"></el-table-column>
+        <el-table-column property="contrats" label="CONTRATS" show-overflow-tooltip sortable width="100"></el-table-column>
+        <el-table-column property="beneficiaire" label="ASSURES / BENEFICIAIRES" show-overflow-tooltip width="179"></el-table-column>
+        <el-table-column property="cotisations" label="COTISATIONS" show-overflow-tooltip sortable width="120"></el-table-column>
+        <el-table-column property="prestations" label="PRESTATIONS" show-overflow-tooltip sortable width="120"></el-table-column>
+        <el-table-column property="tauxTeletransmissions" label="TAUX TELETRANSMISSION" show-overflow-tooltip sortable><template scope="scope"><el-progress :text-inside="true" :stroke-width="18" :percentage="scope.row.tauxTeletransmissions"></el-progress></template></el-table-column>
+        <el-table-column property="documents" label="DOCUMENTS" show-overflow-tooltip width="85" style="text-align: center;"><template scope="scope"><font-awesome-icon v-if="scope.row.documents" icon="download" class="size-export"/></template></el-table-column>
+      </el-table>
+    </div>
+    <el-button @click="test"></el-button>
+
+
   </div>
 </template>
 
@@ -26,76 +43,23 @@ export default {
   name: 'Sante',
   data () {
     return {
-      tableData3: [{
-          nom: 'Holding 1',
-          entreprises: 5,
-          contrats: 14,
-          assuresBeneficiaires: '5/5',
-          cotisations: 485162,
-          prestations:756985,
-          tauxTeletransmissions:35,
-          documents: 0,
-        }, {
-          nom: 'Holding 2',
-          entreprises: 5,
-          contrats: 7,
-          assuresBeneficiaires: '5/5',
-          cotisations: 153248,
-          prestations: 153248,
-          tauxTeletransmissions:42,
-          documents: 3,
-        }, {
-          nom: 'Holding 3',
-          entreprises: 7,
-          contrats: 17,
-          assuresBeneficiaires: 2/2,
-          cotisations: 365214,
-          prestations: 852645,
-          tauxTeletransmissions: 36,
-          documents: 3,
-        }, {
-          nom: 'Holding 4',
-          entreprises: 5,
-          contrats: 5,
-          assuresBeneficiaires: '5/5',
-          cotisations: 458965,
-          prestations: 215632,
-          tauxTeletransmissions: 78,
-          documents: 0,
-        }, {
-          nom: 'Holding 5',
-          entreprises: 5,
-          contrats: 5,
-          assuresBeneficiaires: '5/5',
-          cotisations: 785498,
-          prestations: 463812,
-          tauxTeletransmissions: 89,
-          documents: 3,
-        }, {
-          nom: 'Holding 6',
-          entreprises: 1,
-          contrats: 5,
-          assuresBeneficiaires: '5/5',
-          cotisations: 951357,
-          prestations: 357159,
-          tauxTeletransmissions: 100,
-          documents: 0,
-        }, {
-          nom: 'Holding 7',
-          entreprises: 71,
-          contrats: 85,
-          assuresBeneficiaires: '5/5',
-          cotisations: 456265,
-          prestations: 742358,
-          tauxTeletransmissions: 10,
-          documents: 3,
-        }],
-        multipleSelection: []
+      holdings: this.$store.state.holdings,
+      activeIndex: '',
+      entreprises: '',
+      multipleSelection: [],
+      holdingTable: true,
+      entrepriseTable: false,
     }
   },
   props: {
   },
   methods: {
+    showEntreprise (param) {
+      console.log(param, 'hello')
+      this.entreprises = param
+      this.holdingTable = false
+      this.entrepriseTable = true
+    },
     toggleSelection(rows) {
       if (rows) {
         rows.forEach(row => {
@@ -107,6 +71,12 @@ export default {
     },
     handleSelectionChange(val) {
       this.multipleSelection = val;
+    },
+    test () {
+      console.log(this.holdings)
+      console.log(this.holdings[1].entreprises)
+      this.holdingTable = true
+      this.entrepriseTable = false
     }
   }
 }
@@ -134,5 +104,9 @@ export default {
 }
 .table-wrapping {
   padding-left: 40px;
+}
+.data-wrapper {
+  width: 100%;
+  height: 100%;
 }
 </style>
--- src/store/Store.js
+++ src/store/Store.js
@@ -6,6 +6,140 @@ Vue.use(Vuex)
 export const store = new Vuex.Store({
     state: {
         authenticated: "",
+        holdings: [
+            {
+                name: 'Holding 1',
+                cotisations:'456 123',
+                prestations: '789 159',
+                assure: '5',
+                Beneficiaire: '2',
+                contrats: '15',
+                tauxTeletransmissions: 35,
+                documents: 0,
+                entreprises:[
+                    {
+                        name: 'Entreprise 1',
+                        typeContrat: 'Santé base',
+                        contrats: 'Option X',
+                        debut: '01/01/2018',
+                        fin: '05/04/2018',
+                        montant: '132 465',
+                        status: 'Payé',
+                        declarationDsn: 'Oui',
+                        reglement: 'Virement',
+                        derniereRelance: '01/01/2018',
+                    },
+                    {
+                        name: 'Entreprise 2',
+                        typeContrat: 'Santé base',
+                        contrats: 'Option Y',
+                        debut: '01/01/2018',
+                        fin: '05/04/2018',
+                        montant: '456 123',
+                        status: 'Payé',
+                        declarationDsn: 'Oui',
+                        reglement: 'Virement',
+                        derniereRelance: '01/08/2018',
+                    },
+                    {
+                        name: 'Entreprise 3',
+                        typeContrat: 'Santé base',
+                        contrats: 'Ensemble personnel N° xxx RL',
+                        debut: '01/01/2018',
+                        fin: '05/04/2018',
+                        montant: '159 263',
+                        status: 'Payé',
+                        declarationDsn: 'Oui',
+                        reglement: 'Virement',
+                        derniereRelance: '01/01/2018',
+                    }
+                ]
+            },
+            {
+                name: 'Holding 2',
+                cotisations: '478 965',
+                prestations: '123 625',
+                assure: '3',
+                Beneficiaire: '5',
+                contrats: '14',
+                tauxTeletransmissions: 75,
+                documents: 5,
+                entreprises: [
+                    {
+                        name: 'Entreprise 1',
+                        typeContrat: 'Santé base',
+                        contrats: 'Ensemble personnel N° xxx RL',
+                        debut: '01/01/2018',
+                        fin: '05/04/2018',
+                        montant: '789 451',
+                        status: 'Impayé',
+                        declarationDsn: 'Oui',
+                        reglement: 'Virement',
+                        derniereRelance: '01/01/2018',
+                    },
+                    {
+                        name: 'Entreprise 2',
+                        typeContrat: 'Santé base',
+                        contrats: 'Option X',
+                        debut: '01/01/2018',
+                        fin: '05/04/2018',
+                        montant: '698 458',
+                        status: 'Impayé',
+                        declarationDsn: 'Oui',
+                        reglement: 'Virement',
+                        derniereRelance: '01/01/2018',
+                    }
+                ]
+            },
+            {
+                name: 'Holding 3',
+                cotisations: '852 945',
+                prestations: '425 756',
+                assure: '1',
+                Beneficiaire: '8',
+                contrats: '9',
+                tauxTeletransmissions: 55,
+                documents: 2,
+                entreprises: [
+                    {
+                        name: 'Entreprise 1',
+                        typeContrat: 'Santé base',
+                        contrats: 'Option X',
+                        debut: '01/01/2018',
+                        fin: '05/04/2018',
+                        montant: '745 652',
+                        status: 'Payé',
+                        declarationDsn: 'Oui',
+                        reglement: 'Virement',
+                        derniereRelance: '01/01/2018',
+                    },
+                    {
+                        name: 'Entreprise 2',
+                        typeContrat: 'Santé base',
+                        contrats: 'Ensemble personnel N° xxx RL',
+                        debut: '01/01/2018',
+                        fin: '05/04/2018',
+                        montant: '785 621',
+                        status: 'Payé',
+                        declarationDsn: 'Oui',
+                        reglement: 'Virement',
+                        derniereRelance: '01/01/2018',
+                    },
+                    {
+                        name: 'Entreprise 3',
+                        typeContrat: 'Santé base',
+                        contrats: 'Ensemble personnel N° xxx RL',
+                        debut: '01/01/2018',
+                        fin: '05/04/2018',
+                        montant: '125 984',
+                        status: 'Payé',
+                        declarationDsn: 'Oui',
+                        reglement: 'Virement',
+                        derniereRelance: '01/01/2018',
+                    }
+                ]
+            }
+        ],
         mutations: {
 
         }
