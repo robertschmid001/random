4953247321afac42499a8b914cd925efa0d20ca7
Robert.SCHMID <robertschmid001@gmail.com>
2018-10-10 17:23:09 +0200

    added more data and dynamic table

--- src/components/Portefeuille/_subs/Sante.vue
+++ src/components/Portefeuille/_subs/Sante.vue
@@ -9,12 +9,12 @@
       <el-table ref="multipleTable" :data="holdings" style="width: 100%; font-size: 10px;" @selection-change="handleSelectionChange" :stripe="true" :highlight-current-row="true" :default-sort = "{prop: 'nom', order: 'ascending'}">
         <el-table-column type="selection" width="42"></el-table-column>
         <el-table-column property="name" label="NOM" sortable width="100"><template scope="scope" >{{scope.row.name}}</template></el-table-column>
-        <el-table-column property="entreprises.length" label="ENTREPRISES" sortable width="120"><template scope="scope" ><div @click="showEntreprise(scope.row.entreprises)" class="data-wrapper">{{scope.row.entreprises.length}}</div></template></el-table-column>
-        <el-table-column property="contrats" label="CONTRATS" show-overflow-tooltip sortable width="100"><template scope="scope" ><div @click="showContrats(scope.row.entreprises)" class="data-wrapper">{{scope.row.contrats}}</div></template></el-table-column>
+        <el-table-column property="entreprises.length" label="ENTREPRISES" sortable width="120"><template scope="scope" ><div @click="showEntreprise(scope.row.entreprises)" class="data-wrapper pointer">{{scope.row.entreprises.length}}</div></template></el-table-column>
+        <el-table-column property="contrats" label="CONTRATS" show-overflow-tooltip sortable width="100"><template scope="scope" ><div @click="showContrats(scope.row.entreprises)" class="data-wrapper pointer">{{contratsLength(scope.row.entreprises)}}</div></template></el-table-column>
         <el-table-column property="beneficiaire" label="ASSURES / BENEFICIAIRES" show-overflow-tooltip width="179"></el-table-column>
-        <el-table-column property="cotisations" label="COTISATIONS" show-overflow-tooltip sortable width="120"></el-table-column>
-        <el-table-column property="prestations" label="PRESTATIONS" show-overflow-tooltip sortable width="120"></el-table-column>
-        <el-table-column property="tauxTeletransmissions" label="TAUX TELETRANSMISSION" show-overflow-tooltip sortable><template scope="scope"><el-progress :text-inside="true" :stroke-width="18" :percentage="scope.row.tauxTeletransmissions"></el-progress></template></el-table-column>
+        <el-table-column property="totalCotisations" label="COTISATIONS" show-overflow-tooltip sortable width="120"></el-table-column>
+        <el-table-column property="totalPrestations" label="PRESTATIONS" show-overflow-tooltip sortable width="120"></el-table-column>
+        <el-table-column property="totalTauxTele" label="TAUX TELETRANSMISSION" show-overflow-tooltip sortable><template scope="scope"><el-progress :text-inside="true" :stroke-width="18" :percentage="scope.row.totalTauxTele"></el-progress></template></el-table-column>
         <el-table-column property="documents" label="DOCUMENTS" show-overflow-tooltip width="85" style="text-align: center;"><template scope="scope"><font-awesome-icon v-if="scope.row.documents" icon="download" class="size-export"/></template></el-table-column>
       </el-table>
     </div>
@@ -22,7 +22,7 @@
       <el-table ref="multipleTable" :data="activeEntreprise" style="width: 100%; font-size: 10px;" @selection-change="handleSelectionChange" :stripe="true" :highlight-current-row="true" :default-sort = "{prop: 'nom', order: 'ascending'}">
         <el-table-column type="selection" width="42"></el-table-column>
         <el-table-column property="name" label="ENTREPRISES" sortable width="120"></el-table-column>
-        <el-table-column property="contrats" label="CONTRATS" show-overflow-tooltip sortable width="100"></el-table-column>
+        <el-table-column property="contrats" label="CONTRATS" show-overflow-tooltip sortable width="100"><template scope="scope" ><div class="data-wrapper pointer">{{contratsLength(scope.row)}}</div></template></el-table-column>
         <el-table-column property="beneficiaire" label="ASSURES / BENEFICIAIRES" show-overflow-tooltip width="179"></el-table-column>
         <el-table-column property="cotisations" label="COTISATIONS" show-overflow-tooltip sortable width="120"></el-table-column>
         <el-table-column property="prestations" label="PRESTATIONS" show-overflow-tooltip sortable width="120"></el-table-column>
@@ -33,7 +33,15 @@
     <div class="table-wrapping" v-show="contratsTable">
       <el-table ref="multipleTable" :data="entrContrats" style="width: 100%; font-size: 10px;" @selection-change="handleSelectionChange" :stripe="true" :highlight-current-row="true" :default-sort = "{prop: 'nom', order: 'ascending'}">
         <el-table-column type="selection" width="42"></el-table-column>
-        <el-table-column property="name" label="Name" sortable width="100"><template scope="scope" ><div class="data-wrapper">{{scope.row.name}} test</div></template></el-table-column>
+        <el-table-column property="contrat" label="CONTRAT" sortable width="100"><template scope="scope" ><div class="data-wrapper">{{scope.row.contrat}}</div></template></el-table-column>
+        <el-table-column property="type" label="TYPE CONTRAT" sortable width="100"><template scope="scope" ><div class="data-wrapper">{{scope.row.type}}</div></template></el-table-column>
+        <el-table-column property="assureur" label="ASSUREUR" sortable width="100"><template scope="scope" ><div class="data-wrapper">{{scope.row.assureur}}</div></template></el-table-column>
+        <el-table-column property="reseauDeSoin" label="RESEAU DE SOIN" sortable width="100"><template scope="scope" ><div class="data-wrapper">{{scope.row.reseauDeSoin}}</div></template></el-table-column>
+        <el-table-column property="debut" label="DEBUT" sortable width="100"><template scope="scope" ><div class="data-wrapper">{{scope.row.debut}}</div></template></el-table-column>
+        <el-table-column property="fin" label="FIN" sortable width="100"><template scope="scope" ><div class="data-wrapper">{{scope.row.fin}}</div></template></el-table-column>
+        <el-table-column property="cotisations" label="COTISATIONS" sortable width="100"><template scope="scope" ><div class="data-wrapper">{{scope.row.cotisations}}</div></template></el-table-column>
+        <el-table-column property="prestations" label="PRESTATIONS" sortable width="100"><template scope="scope" ><div class="data-wrapper">{{scope.row.prestations}}</div></template></el-table-column>
+        <el-table-column property="tauxCotisations" label="TAUX DE COTISATIONS" sortable width="100"><template scope="scope" ><el-progress :text-inside="true" :stroke-width="18" :percentage="scope.row.tauxCotisations"></el-progress></template></el-table-column>
         <!-- <el-table-column property="entreprises.contrats" label="type" sortable width="100"></el-table-column> -->
       </el-table>
     </div>
@@ -59,16 +67,17 @@ export default {
       contratsTable: false,
       entreContrats: [],
       entrContrats: [],
+      activeC: []
     }
   },
   props: {
   },
+  computed: {
+  },
   methods: {
     showEntreprise (param) {
-      console.log(param, 'showEntreprise')
       this.activeEntreprise = param
       this.entreContrats = this.activeEntreprise
-      console.log(this.entreContrats, 'contrats')
       this.holdingTable = false
       this.entrepriseTable = true
     },
@@ -76,7 +85,16 @@ export default {
       this.holdingTable = false
       this.entrepriseTable = false
       this.contratsTable = true
-      console.log(param, 'showContrats')
+
+      // this.entrContrats = param
+
+      for (var i = 0; i < param.length; i++) {
+        for (var y = 0; y < param[i].contrats.length; y++) {
+        this.entrContrats.push(param[i].contrats[y])
+        console.log(this.entrContrats)
+        }
+      }
+      return this.entrContrats
     },
 
     toggleSelection(rows) {
@@ -92,13 +110,23 @@ export default {
       this.multipleSelection = val;
     },
     test () {
-      console.log(this.holdings)
-      console.log(this.holdings[1].entreprises)
       this.holdingTable = true
       this.entrepriseTable = false
+      this.contratsTable = false
+    },
+    contratsLength (param) {
+      var holdingContrats = [];
+      for (var i = 0; i < param.length; i++) {
+        for (var y = 0; y < param[i].contrats.length; y++) {
+        holdingContrats.push(param[i].contrats[y])
+        }
+      }
+      return holdingContrats.length;
+    },
+  },
+    created () {
     }
   }
-}
 </script>
 
 <style lang="scss" scoped>
--- src/store/Store.js
+++ src/store/Store.js
@@ -10,6 +10,9 @@ export const store = new Vuex.Store({
         holdings: [
             {
                 name: 'Holding 1',
+                totalCotisations: '85841',
+                totalPrestations: '75853',
+                totalTauxTele: '35',
                 entreprises: [
                     {
                         name: 'Entreprise 1',
@@ -17,7 +20,7 @@ export const store = new Vuex.Store({
                         adresse: '6 rue de la farandole 29069 A-Poile',
                         contrats: [
                             {
-                                contrat: 'ensemble etc..',
+                                contrat: 'ensemble etc.. 1',
                                 type: 'santé base',
                                 assureur: 'Assureur 1',
                                 reseauDeSoin: 'Santé clair',
@@ -38,7 +41,7 @@ export const store = new Vuex.Store({
                                 }]
                             },
                             {
-                                contrat: 'cadre',
+                                contrat: 'cadre 2',
                                 type: 'type X',
                                 assureur: 'Assureur 1',
                                 reseauDeSoin: 'Santé clair',
@@ -76,7 +79,7 @@ export const store = new Vuex.Store({
                         NumSiret: 'FR6355214450300018',
                         adresse: '5 rue de la fanfare 69069 Tout-nu',
                         contrats: [{
-                                contrat: 'ensemble etc..',
+                                contrat: 'ensemble etc.. 3',
                                 type: 'santé base',
                                 assureur: 'Assureur 2',
                                 reseauDeSoin: 'Santé clair',
@@ -97,7 +100,7 @@ export const store = new Vuex.Store({
                                 }]
                             },
                             {
-                                contrat: 'cadre',
+                                contrat: 'cadre 4',
                                 type: 'type X',
                                 assureur: 'Assureur 2',
                                 reseauDeSoin: 'Santé clair',
@@ -136,7 +139,7 @@ export const store = new Vuex.Store({
                         adresse: '5 rue de la fanfare 69069 Tout-nu',
                         contrats: [
                             {
-                                contrat: 'ensemble etc..',
+                                contrat: 'ensemble etc.. 5',
                                 type: 'santé base',
                                 assureur: 'Assureur 3',
                                 reseauDeSoin: 'Santé clair',
@@ -155,6 +158,111 @@ export const store = new Vuex.Store({
                                         teletransmission: 'Message',
                                         carte: '13/02/2017',
                                         Documents: '5',
+                                    }
+                                ]
+                            }
+                        ]
+                    }
+                ]
+            },
+            {
+                name: 'Holding 2',
+                totalCotisations: '98753',
+                totalPrestations: '87456',
+                totalTauxTele: '35',
+                entreprises: [{
+                        name: 'Entreprise 5',
+                        NumSiret: 'FR6355214450300018',
+                        adresse: '6 rue de la farandole 29069 A-Poile',
+                        contrats: [{
+                                contrat: 'ensemble etc.. 1',
+                                type: 'santé base',
+                                assureur: 'Assureur 1',
+                                reseauDeSoin: 'Santé clair',
+                                debut: '28/02/2018',
+                                fin: '02/12/2019',
+                                cotisations: '12344',
+                                prestations: '17489',
+                                tauxCotisations: '34',
+                                assures: [{
+                                    name: 'robert Schmid',
+                                    birthdate: '28/02/1985',
+                                    numSecu: '12415615616U5',
+                                    beneficiaires: '6',
+                                    regime: 'général',
+                                    teletransmission: 'Message',
+                                    carte: '13/02/2017',
+                                    Documents: '5',
+                                }]
+                            },
+                            {
+                                contrat: 'cadre 2',
+                                type: 'type X',
+                                assureur: 'Assureur 1',
+                                reseauDeSoin: 'Santé clair',
+                                debut: '02/01/2018',
+                                fin: '13/05/2019',
+                                cotisations: '12574',
+                                prestations: '85489',
+                                tauxCotisations: '25',
+                                assures: [{
+                                        name: 'andrew Germain',
+                                        birthdate: '28/02/1985',
+                                        numSecu: '12415615616U5',
+                                        beneficiaires: '2',
+                                        regime: 'général',
+                                        teletransmission: 'Message',
+                                        carte: '13/02/2017',
+                                        Documents: '3',
+                                    },
+                                    {
+                                        name: 'timmy Cup',
+                                        birthdate: '28/85/1990',
+                                        numSecu: '124141565616U5',
+                                        beneficiaires: '8',
+                                        regime: 'général',
+                                        teletransmission: 'Message',
+                                        carte: '13/02/2017',
+                                        Documents: '9',
+                                    }
+                                ]
+                            }
+                        ]
+                    },
+                    {
+                        name: 'Entreprise 6',
+                        NumSiret: 'FR6355214450300018',
+                        adresse: '5 rue de la fanfare 69069 Tout-nu',
+                        contrats: [
+                            {
+                                contrat: 'cadre 4',
+                                type: 'type X',
+                                assureur: 'Assureur 2',
+                                reseauDeSoin: 'Santé clair',
+                                debut: '02/01/2018',
+                                fin: '13/05/2019',
+                                cotisations: '12574',
+                                prestations: '85489',
+                                tauxCotisations: '25',
+                                assures: [{
+                                        name: 'andrew GermainAAA',
+                                        birthdate: '28/02/1985',
+                                        numSecu: '12415615616U5',
+                                        beneficiaires: '2',
+                                        regime: 'général',
+                                        teletransmission: 'Message',
+                                        carte: '13/02/2017',
+                                        Documents: '3',
+                                    },
+                                    {
+                                        name: 'timmy CupAAA',
+                                        birthdate: '28/85/1990',
+                                        numSecu: '124141565616U5',
+                                        beneficiaires: '8',
+                                        regime: 'général',
+                                        teletransmission: 'Message',
+                                        carte: '13/02/2017',
+                                        Documents: '9',
                                     }
                                 ]
                             }
